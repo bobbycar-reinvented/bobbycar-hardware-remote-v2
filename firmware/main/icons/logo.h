303ea8adb379365d2cae3d5e56344843d641bbce
CommanderRedYT <ccommanderred@gmail.com>
2022-06-30 00:43:12 +0200

    Added bobbyicons

---
+++ firmware/main/icons/logo.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "icon.h"
+
+namespace bobbyicons {
+extern const espgui::Icon<240, 130> logo;
+} // namespace bobbyicons
---
+++ firmware/main/icons/shortcircuit.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "icon.h"
+
+namespace bobbyicons {
+extern const espgui::Icon<220, 137> shortcircuit;
+} // namespace bobbyicons
